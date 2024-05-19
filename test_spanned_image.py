1cecd2d923f819fbad41c305ccfe228ea4e92c13
Isaac A. Nugroho <>
2024-05-20 02:20:29 +0700

    fix test

--- test_spanned_image.py
+++ test_spanned_image.py
@@ -13,19 +13,17 @@ class Test(TestCase):
         'a': DisplayInfo(
             Monitor(name='a', x=0, y=0, width=800, height=600, width_mm=8000, height_mm=6000)),
         'b': DisplayInfo(
-            Monitor(name='b', x=1024, y=0, width=1024, height=768, width_mm=10240, height_mm=7680),
-            mm_offset_x=5),
+            Monitor(name='b', x=1024, y=0, width=1024, height=768, width_mm=10240, height_mm=7680)),
         'c': DisplayInfo(
-            Monitor(name='c', x=0, y=600, width=1024, height=768, width_mm=10240, height_mm=7680),
-            mm_offset_x=5),
+            Monitor(name='c', x=0, y=600, width=1024, height=768, width_mm=10240, height_mm=7680)),
     }
     monitor_b = monitors['b']
-    left_of_b = find_display_left(monitor_b, monitors)
+    left_of_b = find_display_left(monitor_b, list(monitors.values()))
     print(left_of_b)
-    assert left_of_b == 'c'
+    assert left_of_b.name == 'a'
 
     monitor_c = monitors['c']
-    left_of_c = find_display_left(monitor_c, monitors)
+    left_of_c = find_display_left(monitor_c, list(monitors.values()))
     print(left_of_c)
     assert left_of_c is None
 
@@ -35,15 +33,13 @@ class Test(TestCase):
         'a': DisplayInfo(
             Monitor(name='a', x=0, y=0, width=800, height=600, width_mm=8000, height_mm=6000)),
         'b': DisplayInfo(
-            Monitor(name='b', x=800, y=0, width=1024, height=768, width_mm=10240, height_mm=7680),
-            mm_offset_x=5),
+            Monitor(name='b', x=800, y=0, width=1024, height=768, width_mm=10240, height_mm=7680)),
         'c': DisplayInfo(
-            Monitor(name='c', x=1824, y=0, width=1024, height=768, width_mm=10240, height_mm=7680),
-            mm_offset_x=10),
+            Monitor(name='c', x=1824, y=0, width=1024, height=768, width_mm=10240, height_mm=7680)),
     }
     normalize_displays(monitors)
-    assert monitors['b'].mm_x == 8005
-    assert monitors['c'].mm_x == 18255
+    assert monitors['b'].mm_x == 8000
+    assert monitors['c'].mm_x == 18240
     print(str(monitors))
 
 
